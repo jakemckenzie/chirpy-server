fcd81a240098b5e6efd4c274e87e83c976a5481d
Jake McKenzie <jake314@uw.edu>
2025-04-22 05:04:59 -0600

    chapter 1 complete

--- main.go
+++ main.go
@@ -4,10 +4,19 @@ import (
 	"net/http"
 )
 
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.HandleFunc("/healthz", readinessHandler)
+
+	fileServerHandler := http.FileServer(http.Dir("."))
+	mux.Handle("/app/", http.StripPrefix("/app", fileServerHandler))
 
 	server := http.Server{
 		Addr: ":8080",
