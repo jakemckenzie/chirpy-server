98dbba0e9565fd091c3d70d3b3375542faecc4d7
Jake McKenzie <jake314@uw.edu>
2025-04-24 01:23:02 -0600

    chore:ch5L4 done

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,48 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
+	"os"
 
+	_ "github.com/lib/pq"
+
+	"github.com/jakemckenzie/chirpy-server/internal/config"
+	"github.com/jakemckenzie/chirpy-server/internal/database"
 	"github.com/jakemckenzie/chirpy-server/internal/handlers"
 	"github.com/jakemckenzie/chirpy-server/internal/middleware"
 	"github.com/jakemckenzie/chirpy-server/internal/services"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file:", err)
+	}
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL not set in .env")
+	}
+
+	dbConn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+	defer dbConn.Close()
+
 	ms := services.NewMetricsService()
 	ts := services.NewTextService()
+	dbQueries := database.New(dbConn)
+
+	cfg := &config.APIConfig{
+		MetricsService: ms,
+		TextService:    ts,
+		DBQueries:      dbQueries,
+	}
 
 	mux := http.NewServeMux()
 	fileServerHandler := http.FileServer(http.Dir("../../static"))
@@ -21,14 +53,17 @@ func main() {
 		http.Redirect(w, r, "/app/", http.StatusMovedPermanently)
 	})
 
-	mux.HandleFunc("/api/healthz", handlers.ReadinessHandler)
-	mux.HandleFunc("/admin/metrics", handlers.AdminMetricsHandler(ms))
-	mux.HandleFunc("/admin/reset", handlers.ResetHandler(ms))
-	mux.HandleFunc("/api/validate_chirp", handlers.ValidateChirpHandler(ts))
+	mux.HandleFunc("/api/healthz", handlers.ReadinessHandler(cfg))
+	mux.HandleFunc("/admin/metrics", handlers.AdminMetricsHandler(cfg))
+	mux.HandleFunc("/admin/reset", handlers.ResetHandler(cfg))
+	mux.HandleFunc("/api/validate_chirp", handlers.ValidateChirpHandler(cfg))
+	mux.HandleFunc("/api/users", handlers.CreateUserHandler(cfg))
 
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
+
+	log.Println("Server starting on :8080...")
 	server.ListenAndServe()
 }
